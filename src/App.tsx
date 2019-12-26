5da2e8e94500dc69ba533877ad3d30b51fe1b344
martinyounghoonkim <hoons0131@gmail.com>
2019-12-26 22:57:26 +0900

    API를 통해 TODOS 데이터 받아오는 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 import './App.css';
 import Information from "./Information";
+import axios from "axios";
+import { ITodo } from "./todo";
 
 const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = React.useState<ITodo[]>([]);
   const [text, setText] = React.useState("");
+  React.useEffect(() => {
+    axios.get("https://todo-backend-modern-js.herokuapp.com/todos")
+      .then(res => {
+        setTodos(res.data);
+      });
+  }, []); // 만약 빈 배열을 넣어주지 않는다면, 텍스트를 입력할 때마다 API를 호출하게 된다.
   return (
     <div className="App">
       <div>
@@ -13,6 +22,11 @@ const App: React.FunctionComponent = () => {
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
         />
       </div>
+      <ul>
+        {todos.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
       <Information isTyping={text}/>
     </div>
   );
---
+++ src/todo.d.ts
@@ -0,0 +1,6 @@
+export interface ITodo {
+  title: string;
+  url: string;
+  id: string;
+  completed: boolean;
+}
