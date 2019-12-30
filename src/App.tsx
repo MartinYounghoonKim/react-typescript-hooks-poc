7c1de854ea581e6d8d58c68ecdd74a49e8d0d78c
martinyounghoonkim <hoons0131@gmail.com>
2019-12-30 22:12:31 +0900

    삭제, 추가, 패치 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,32 @@ import './App.css';
 import Information from "./Information";
 import axios from "axios";
 import { ITodo } from "./todo";
+import { useEffect } from "react";
 
 const App: React.FunctionComponent = () => {
   const [todos, setTodos] = React.useState<ITodo[]>([]);
   const [text, setText] = React.useState("");
-  React.useEffect(() => {
-    axios.get("https://todo-backend-modern-js.herokuapp.com/todos")
-      .then(res => {
-        setTodos(res.data);
-      });
+  const deleteTodo = async (id: string) => {
+    const response  = await axios.delete(`https://todo-backend-modern-js.herokuapp.com/todos/${id}`);
+    if (response.status === 200) {
+      setTodos(todos.filter(v => v.id !== id))
+    }
+  };
+  const addTodo = async (title: string) => {
+    const response  = await axios.post("https://todo-backend-modern-js.herokuapp.com/todos", { title });
+    if (response.status === 200) {
+      setTodos(prevState => [...prevState, response.data]);
+    }
+  };
+  const fetchTodos = async () => {
+    const response = await axios.get("https://todo-backend-modern-js.herokuapp.com/todos");
+    if (response.status === 200) {
+      setTodos(response.data);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodos();
   }, []); // 만약 빈 배열을 넣어주지 않는다면, 텍스트를 입력할 때마다 API를 호출하게 된다.
   return (
     <div className="App">
@@ -21,13 +38,18 @@ const App: React.FunctionComponent = () => {
           type="text"
           value={text}
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)}
+          onKeyDown={(e: React.KeyboardEvent) => {
+            if (e.keyCode === 13) {
+              addTodo(text)
+            }
+          }}
         />
       </div>
       <ul>
         {todos.map(({ id, title }) => (
           <li key={id}>
             {title}
-            <button type="button" onClick={() => setTodos(todos.filter(v => v.id !== id))}>X</button>
+            <button type="button" onClick={() => deleteTodo(id)}>X</button>
           </li>
         ))}
       </ul>
